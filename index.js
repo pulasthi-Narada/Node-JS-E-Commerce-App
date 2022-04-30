93270eda981d08cf6e4abc56c39a3edce882f032
Pulasthi nanayakkara <pulasthinarada@hotmail.com>
2022-04-30 11:55:11 +0530

    add bodyParser

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-	res.send('<h1>hi there!</h1>');
-	console.log(req);
+	res.send(` <div>
+  <form method="POST">
+    <input name="email" placeholder="email" />
+    <input name="password" placeholder="password" />
+    <input name="passwordConfirmation" placeholder="password confirmation" />
+    <button>Sign Up</button>
+  </form>
+</div>`);
+	console.log('get');
+});
+
+app.post('/', (req, res) => {
+	console.log(req.body);
+	res.send('Account created!!!');
 });
 
 app.listen(3000, () => {

a086e6ff4afc17aaacfc71683aed23eaf07bb775
Pulasthi nanayakkara <pulasthinarada@hotmail.com>
2022-05-04 19:41:57 +0530

    refactor index.js

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const usersRepo = require('./repositories/users');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-	res.send(` <div>
-  <form method="POST">
-    <input name="email" placeholder="email" />
-    <input name="password" placeholder="password" />
-    <input name="passwordConfirmation" placeholder="password confirmation" />
-    <button>Sign Up</button>
-  </form>
-</div>`);
-});
-
-app.post('/', async (req, res) => {
-	const { email, password, passwordConfirmation } = req.body;
-
-	const existingUser = await usersRepo.getOneBy({ email });
-	if (existingUser) {
-		return res.send('Email in use');
-	}
-
-	if (password !== passwordConfirmation) {
-		return res.send('Passwords must match');
-	}
-
-	// Create a user in our user repo to represent this person
-	const user = await usersRepo.create({ email, password });
-
-	// Store the id of that user inside the users cookie
-
-	res.send('Account created!!!');
-});
+app.use(
+	cookieSession({
+		keys: [ 'lkasld235j' ]
+	})
+);
+app.use(authRouter);
 
 app.listen(3000, () => {
-	console.log('Listening!');
+	console.log('Listening');
 });

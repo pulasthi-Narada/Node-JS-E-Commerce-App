3c8c8c01415020310d48ebef210a36cd007f2cc2
Pulasthi nanayakkara <pulasthinarada@hotmail.com>
2022-05-04 09:58:02 +0530

    Create a user in our user  to represent this person

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.post('/', async (req, res) => {
 		return res.send('Passwords must match');
 	}
 
+	// Create a user in our user repo to represent this person
+	const user = await usersRepo.create({ email, password });
+
+	// Store the id of that user inside the users cookie
+
 	res.send('Account created!!!');
 });
 

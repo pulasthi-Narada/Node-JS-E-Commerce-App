85262a3917dd7fe7537b1b7d70b8e1b2dc75f2ae
Pulasthi nanayakkara <pulasthinarada@hotmail.com>
2022-05-05 10:36:00 +0530

    add validators.js

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,64 +1,55 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
 
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-  res.send(signupTemplate({ req }));
+	res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
+router.post('/signup', [ requireEmail, requirePassword, requirePasswordConfirmation ], async (req, res) => {
+	const errors = validationResult(req);
+	console.log(errors);
 
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('Email in use');
-  }
+	const { email, password, passwordConfirmation } = req.body;
+	const user = await usersRepo.create({ email, password });
 
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
+	req.session.userId = user.id;
 
-  // Create a user in our user repo to represent this person
-  const user = await usersRepo.create({ email, password });
-
-  // Store the id of that user inside the users cookie
-  req.session.userId = user.id;
-
-  res.send('Account created!!!');
+	res.send('Account created!!!');
 });
 
 router.get('/signout', (req, res) => {
-  req.session = null;
-  res.send('You are logged out');
+	req.session = null;
+	res.send('You are logged out');
 });
 
 router.get('/signin', (req, res) => {
-  res.send(signinTemplate());
+	res.send(signinTemplate());
 });
 
 router.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  const user = await usersRepo.getOneBy({ email });
+	const user = await usersRepo.getOneBy({ email });
 
-  if (!user) {
-    return res.send('Email not found');
-  }
+	if (!user) {
+		return res.send('Email not found');
+	}
 
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-  if (!validPassword) {
-    return res.send('Invalid password');
-  }
+	const validPassword = await usersRepo.comparePasswords(user.password, password);
+	if (!validPassword) {
+		return res.send('Invalid password');
+	}
 
-  req.session.userId = user.id;
+	req.session.userId = user.id;
 
-  res.send('You are signed in!!!');
+	res.send('You are signed in!!!');
 });
 
 module.exports = router;

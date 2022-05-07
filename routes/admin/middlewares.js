7a7e42ee9ac8e2f6491a34bdf7ecfe8dda3200f9
Pulasthi nanayakkara <pulasthinarada@hotmail.com>
2022-05-07 11:38:51 +0530

    fix :  Authentication and Template Update

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -11,5 +11,12 @@ module.exports = {
 
       next();
     };
+  },
+  requireAuth(req, res, next) {
+    if (!req.session.userId) {
+      return res.redirect('/signin');
+    }
+
+    next();
   }
 };

3ea0892af7ec8b9d2561e573aeb2e3fda4c0e15a
Pulasthi nanayakkara <pulasthinarada@hotmail.com>
2022-05-07 11:12:04 +0530

    fix : redirect url

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -28,7 +28,7 @@ router.post(
 
     req.session.userId = user.id;
 
-    res.send('Account created!!!');
+    res.redirect('/admin/products');
   }
 );
 
@@ -52,7 +52,7 @@ router.post(
 
     req.session.userId = user.id;
 
-    res.send('You are signed in!!!');
+    res.redirect('/admin/products');
   }
 );
 

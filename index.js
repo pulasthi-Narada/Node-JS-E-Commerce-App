8daf4d4d639a27c9fcc2f98bce999d03e54cdf59
Pulasthi nanayakkara <pulasthinarada@hotmail.com>
2022-05-08 09:02:41 +0530

    fix produts listing

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
 const app = express();
 
@@ -15,6 +17,8 @@ app.use(
 );
 app.use(authRouter);
 app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
